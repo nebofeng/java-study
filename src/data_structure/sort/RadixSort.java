c19759a91a066b59a852301f4b839790ac5e4246
ningbo <1624647547@qq.com>
2018-02-28 16:33:40 +0800

    --基数，桶，计数排序类 =》增加计数排序

--- src/data_structure/sort/RadixSort.java
+++ src/data_structure/sort/RadixSort.java
@@ -16,8 +16,7 @@ public class RadixSort {
 	   * @param  bit 需要排序的数组最大数的位数
 	   */
 	  private static void  sortCore1(int arr[] ,int bit) {
-		 
-		   
+		 		   
 		  int m=1;
 		  int b =1;
 		  int k =0;
@@ -28,8 +27,7 @@ public class RadixSort {
 		  //从低位开始向高位
 		  
 		  while(m<=bit){
-			  
-	 	  for(int i=0;i<arr.length;i++) {
+		 	  for(int i=0;i<arr.length;i++) {
 				  int lsd = (arr[i]/b)% 10 ;
 				  tmp [lsd][intNumber[lsd]] =arr[i];
 				  intNumber[lsd]++;
@@ -37,29 +35,66 @@ public class RadixSort {
 			  }
 			  for(int j=0;j<10;j++){
 				  if(intNumber[j]!=0) {
-					  for(int i=0;i<intNumber[j];i++) {
-						 System.out.println("==k"+k+"j======="+j+"i=="+i);
-						  arr[k++]=tmp[j][i];  
-						  System.out.println("==========");
+					  for(int i=0;i<intNumber[j];i++) {						 
+						  arr[k++]=tmp[j][i];  						  
 					  }
 					  intNumber[j]=0;
 		 		  }
 			  }
 			  b=b*10;
 			  k=0;
-			  m++;
-			  
+			  m++;			  
 		  }
 	 	  
 	  }
+	  /**
+	   * 计数排序： 辅助数组的大小取决于原数组的值的范围
+	   *  适合值类密集排序	  
+	   * @param arr
+	   */
+	  private static void countSort(int  arr[]) {
+		  int max = Integer.MIN_VALUE;
+		  int min = Integer.MAX_VALUE;
+		  int index = 0;
+		  for(int i=0;i<arr.length;i++) {
+			  max = Math.max(max, arr[i]);
+			  min = Math.min(min, arr[i]);
+			  
+		  }
+		  int help[] = new int[max-min+1];
+		  
+		  for(int i=0;i<arr.length;i++) {
+			  int pos=arr[i]-min ;
+		 
+			  help[pos]++;
+					  
+		  }
+		  
+		  for(int i=0;i<help.length;i++) {
+			  while(help[i]-- > 0) {
+				  arr[index++] = min+i;
+			  }
+		  }
+		  
+		  
+		  
+		  
+	  }
+	  
+	  
 	  
 	  
 	  public static void main(String[] args) {
 		//int arr[]= {34,58,7,656,07222222,06845,0679};
 		 int arr[]= {34,58,7,656, 97222222, 6845, 679};
-		 sortCore1(arr,7);
-		 System.out.println(Arrays.toString(arr));
-		 int age =  800;
+		 int arr1[]= {12,3,4,8,7,43,0,9,8,6,28,28,5,12};
+		 countSort(arr1);
+	//	 sortCore1(arr,7);
+		// System.out.println(Arrays.toString(arr));
+		 System.out.println(Arrays.toString(arr1));
+
+		 
+		// int age =  800;
 
 	}
 	
