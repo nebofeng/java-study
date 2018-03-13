65f8987e3d8f4a9005d9ef15d6a1e39a01a2d666
ningbo <1624647547@qq.com>
2018-03-13 13:57:14 +0800

    --查找第n大的数

---
+++ src/data_structure/search/SearchN.java
@@ -0,0 +1,63 @@
+package data_structure.search;
+
+/**
+ * 
+ * @author *** nebo
+ * @date   *** 2018年3月13日
+ * @email  *** nebofeng@gmail.com
+ * @des    *** 查找第n大的数
+ */
+public class SearchN {
+	public static void main(String[] args) {
+		int arr[] = {1,3,2,4,6};
+		System.out.println(find(arr,0,arr.length-1,2));
+		
+	}
+	
+	
+	
+	
+	
+	                                      //0                       5
+	public static int find(int arr[] ,int low ,int high ,int n) {
+		int right =high;
+		int left=low;
+		if(low==high) {
+			return arr[low];
+		}
+		if(low<high) {
+		    int pov=arr[low];
+		    //0                       5
+			while(low<high) {
+				while(pov<arr[high]&&low<high) {
+					high--;
+				 
+				}
+				arr[low]=arr[high];
+				arr[high]=pov;
+				while(arr[low]<pov&&low<high) {
+					low++;
+				}
+				arr[high]=arr[low];
+				arr[low]=pov;								 
+	 		}
+				if(arr.length-low ==n) {
+				return pov;
+			}else if(arr.length-low>n){
+				System.out.println(high+"==="+right);
+				return find(arr,low+1,right,n);
+			}else {
+				return find(arr,0,low-1,n);
+			}
+		 	
+			
+		}
+		return 0;
+		
+		
+		 
+	}
+	
+	
+
+}
