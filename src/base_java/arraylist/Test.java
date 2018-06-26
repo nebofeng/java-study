460587e0a0510f60be99f7e8f0e74bc20468cebf
ningbo <nebofeng@gmail.com>
2018-06-26 22:10:48 +0800

    --commit4

--- src/base_java/arraylist/Test.java
+++ src/base_java/arraylist/Test.java
@@ -8,6 +8,7 @@ public class Test {
 		 ArrayList a1=new ArrayList();
 		a1.add(9);	a1.add(3);	sop(a1);
 	}	
+
 	public static void sop(Object obj){	System.out.println(obj);
 									  }
 									  
