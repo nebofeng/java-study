2f60b73ef4d55578240b48074bebf51bc477b29f
ningbo <1624647547@qq.com>
2018-06-26 21:56:57 +0800

    --测试commit2

--- src/base_java/arraylist/Test.java
+++ src/base_java/arraylist/Test.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class Test {
 	public static void main(String[] args){
-		ArrayList a1=new ArrayList();	
+
+		ArrayList a1=new ArrayList();
 		a1.add(9);	a1.add(3);	sop(a1);
 	}	
 	public static void sop(Object obj){	System.out.println(obj);
