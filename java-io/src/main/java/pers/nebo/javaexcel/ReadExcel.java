80ae7b1003699386497eeef449005301fe856df7
nebofeng <nebofeng@gmail.com>
2018-09-07 16:49:50 +0800

    --Java解析excel文件示例代码

---
+++ java-io/src/main/java/pers/nebo/javaexcel/ReadExcel.java
@@ -0,0 +1,13 @@
+package pers.nebo.javaexcel;
+
+/**
+ * @auther nebofeng@gmail.com
+ * @date 2018/9/7 15:01
+ */
+public class ReadExcel {
+	public static void main(String[] args) {
+		FileUtils fileUtils = new FileUtils("l_test_log.xlsx");
+
+
+	}
+}
