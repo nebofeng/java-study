dcb9e662e5e28d85022e5d822ceb811a7ba8d55d
nebofeng <nebofeng@gmail.com>
2018-09-11 22:51:23 +0800

    --httpClient 实现http请求

---
+++ java-web/src/main/java/pers/nebo/web/AliYunHttp.java
@@ -0,0 +1,74 @@
+package pers.nebo.web;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.CharArrayBuffer;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @auther nebofeng@gmail.com
+ * @date 2018/9/11 10:19
+ * @des: 阿里云 http请求 。返回 竞价实例的详细信息
+ *
+ */
+public class AliYunHttp {
+	/**
+	 * get请求，参数拼接在地址上
+	 *
+	 * @param url 请求地址加参数
+	 * @return 响应
+	 */
+	public String get(String url) {
+		String result = null;
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet get = new HttpGet(url);
+		CloseableHttpResponse response = null;
+		try {
+			response = httpClient.execute(get);
+			if (response != null && response.getStatusLine().getStatusCode() == 200) {
+				HttpEntity entity = response.getEntity();
+				result = entityToString(entity);
+			}
+			return result;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				httpClient.close();
+				if (response != null) {
+					response.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	private String entityToString(HttpEntity entity) throws IOException {
+		String result = null;
+		if (entity != null) {
+			long lenth = entity.getContentLength();
+			if (lenth != -1 && lenth < 2048) {
+				result = EntityUtils.toString(entity, "UTF-8");
+			} else {
+				InputStreamReader reader1 = new InputStreamReader(entity.getContent(), "UTF-8");
+				CharArrayBuffer buffer = new CharArrayBuffer(2048);
+				char[] tmp = new char[1024];
+				int l;
+				while ((l = reader1.read(tmp)) != -1) {
+					buffer.append(tmp, 0, l);
+				}
+				result = buffer.toString();
+			}
+		}
+		return result;
+	}
+     //请求参数： 	https://ecs.aliyuncs.com/?Action=DescribeInstances&RegionId=cn-hangzhou&<公共请求参数>
+}
