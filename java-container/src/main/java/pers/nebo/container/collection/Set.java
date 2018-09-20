c4ad3336f2654a2a88663cde1b3e4871506f79bd
ningbo <nebofeng@gmail.com>
2018-09-20 23:49:42 +0800

    增加java容器模块

---
+++ java-container/src/main/java/pers/nebo/container/collection/Set.java
@@ -0,0 +1,14 @@
+package pers.nebo.container.collection;
+
+/**
+ * Set的原因是Set集合不包含重复元素，HashSet、TreeSet和LinkedHashSet
+ * 需求是要一个能快速访问的Set，那么就要用HashSet,
+ * 如果你要一个排序Set，那么你应该用TreeSet,
+ * 如果你要记录下插入时的顺序时，你应该使用LinedHashSet
+ */
+public class Set
+
+{
+
+
+}
