d2a151426e29cabd42ad1715667a0598775aca31
nebofeng <nebofeng@gmail.com>
2018-09-27 16:22:10 +0800

    --Java线程学习

---
+++ java-thread/src/main/java/pers/nebo/thread/ThreadDemo.java
@@ -0,0 +1,18 @@
+package pers.nebo.thread;
+
+/**
+ * @auther nebofeng@gmail.com
+ * @date 2018/9/25 16:13
+ */
+
+/**
+ *  java 堆，方法区 是线程共享的区域。
+ *
+ *  程序计数器：
+ *  Java 虚拟机栈： 是线程私有的，他的生命周期与线程相同。虚拟机描述的是 Java方法执行
+ *  的内存模型。 每个方法在执行的同时，都会创建一个栈帧用于存储局部变量表，
+ *
+ */
+public class ThreadDemo {
+
+}
