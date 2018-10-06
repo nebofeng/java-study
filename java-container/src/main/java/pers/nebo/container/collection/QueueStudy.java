74b821d9a4b56d24276b880ca5c8645a7dd0944c
ningbo <nebofeng@gmail.com>
2018-10-06 13:32:56 +0800

    --队列

---
+++ java-container/src/main/java/pers/nebo/container/collection/QueueStudy.java
@@ -0,0 +1,15 @@
+package pers.nebo.container.collection;
+
+import java.util.PriorityQueue;
+
+/**
+ * 队列学习类
+ */
+public class QueueStudy {
+
+    /**
+     PriorityQueue<E> extends AbstractQueue<E>  implements java.io.Serializable
+     */
+    PriorityQueue priorityQueue;
+
+}
