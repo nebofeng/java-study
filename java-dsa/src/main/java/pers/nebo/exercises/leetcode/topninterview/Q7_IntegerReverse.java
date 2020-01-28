cfff554b0020be4b9eb3a2495c3bbb754c1b3210
nebofeng <nebofeng@gmail.com>
2020-01-28 14:37:20 +0800

    [UPDATE] 整数翻转

--- java-dsa/src/main/java/pers/nebo/exercises/leetcode/topninterview/Q7_IntegerReverse.java
+++ java-dsa/src/main/java/pers/nebo/exercises/leetcode/topninterview/Q7_IntegerReverse.java
@@ -32,7 +32,29 @@ package pers.nebo.exercises.leetcode.topninterview;
  */
 public class Q7_IntegerReverse {
 
+    public static void main(String[] args) {
 
+    }
+
+    public static int IntegerReverse(int  source){
+        int ans=0;
+
+        int x=source;
+        while (x!=0){
+
+            int pop= x%10;
+            if(ans>Integer.MAX_VALUE/10 || (ans==Integer.MAX_VALUE/10 && pop >7 ) ){
+                return  0;
+            }
+            if(ans<Integer.MIN_VALUE/10 || (ans==Integer.MAX_VALUE/10 && pop <-8 )){
+               return 0;
+            }
+            ans=ans*10 + pop;
+            x=x/10;
+        }
+        return  ans;
+
+    }
 
 
 
