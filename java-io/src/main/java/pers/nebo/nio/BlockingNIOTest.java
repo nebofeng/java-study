6241c41ef532ed672ccf20a2d417cb1e2c27575b
nebofeng <nebofeng@gmail.com>
2020-12-04 01:56:47 +0800

    [ADD] 阻塞式式网络通信

---
+++ java-io/src/main/java/pers/nebo/nio/BlockingNIOTest.java
@@ -0,0 +1,74 @@
+package pers.nebo.nio;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class BlockingNIOTest {
+    /**
+     * 阻塞式网络通信：
+     *
+     * 1. 通道channel
+     *  Java.nio.channels.Channel
+     *    SockerChannel
+     *    ServerSockerChannel
+     *    DatagramChannel
+     *  管道相关
+     *   Pipe.SinkChannel
+     *   Pipe.SourceChannel
+     *
+     *
+     * 2.缓冲buffer:负责存储数据
+     *
+     * 3.Selector 是SelectableChannel的多路复用器，主要用于监控SelectableChannel的io状况
+     *
+     */
+
+    @Test
+    public  void client(){
+        String filePath="F:\\Video\\美剧\\神秘博士第10季\\神秘博士.doctor.who.2005.s10e09.中英字幕.bd-hr.aac.720p.x264-人人影视.mp4";
+        try {
+            //1.获取通道
+            SocketChannel sChannel=SocketChannel.open(new InetSocketAddress("127.0.0.1",9898));
+
+            // 创建文件通道
+            FileChannel fileChannel=FileChannel.open(Paths.get(filePath), StandardOpenOption.READ);
+
+            //2.分配指定大小的缓冲区
+            ByteBuffer buffer=ByteBuffer.allocate(1024);
+
+            //3. 发送数据需要读取文件
+            while (fileChannel.read(buffer)!=-1){
+                buffer.flip();
+                sChannel.write(buffer);
+                buffer.clear();
+
+            }
+            //4. 关闭通道
+
+            fileChannel.close();
+            sChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Test
+    public  void server(){
+        //1. 获取通道
+
+        //2. 绑定端口
+
+        //
+    }
+
+
+}
