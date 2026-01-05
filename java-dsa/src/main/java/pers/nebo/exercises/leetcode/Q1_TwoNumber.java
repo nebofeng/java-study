4b5323ffca3efbe7f4076396e5514a65adb67474
nebofeng <nebofeng@gmail.com>
2026-01-05 23:10:49 +0800

    [UPDATE] Q1  Two Number

--- java-dsa/src/main/java/pers/nebo/exercises/leetcode/Q1_TwoNumber.java
+++ java-dsa/src/main/java/pers/nebo/exercises/leetcode/Q1_TwoNumber.java
@@ -2,50 +2,110 @@
 package pers.nebo.exercises.leetcode;
 
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
 * @author nebo
 * @E-mail:nebofeng@gmail.com
 * @version creatTime：2017年3月16日 下午6:23:43
 * 类说明
+ *
+ *
+
+
+ *
 */
 public class Q1_TwoNumber {
+
+    /*
+    1. 两数之和
+        已解答
+        简单
+        相关标签
+        premium lock icon
+        相关企业
+        提示
+        给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+        你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+
+        你可以按任意顺序返回答案。
+
+
+
+        示例 1：
+
+        输入：nums = [2,7,11,15], target = 9
+        输出：[0,1]
+        解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+        示例 2：
+
+        输入：nums = [3,2,4], target = 6
+        输出：[1,2]
+        示例 3：
+
+        输入：nums = [3,3], target = 6
+        输出：[0,1]
+
+
+        提示：
+
+        2 <= nums.length <= 104
+        -109 <= nums[i] <= 109
+        -109 <= target <= 109
+        只会存在一个有效答案
+
+
+        进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
+    *
+    *
+    *
+    * */
 	//n的方
-	public void twonumber(int[] array,int s){
+	public static int[] twoNumber(int[] array,int s){
+        int [] result = new int[2];
 		for(int i=0;i<array.length;i++){
 			for(int j=i+1;j<array.length;j++){
 				if(array[i]+array[j]==s){
-					System.out.println(array[i]+"||"+array[j]);
-					System.out.println(""+s);
-					break;
+                    result[0]=i;
+                    result[1]=j;
+					return  result;
 				}
 			}
 		}
-	}
-	
-	//n
-	
-	
-	public void twonumber2(int[] array,int s){
-		int i=0,j=array.length-1;
-		while(i<j){
-			int sum=array[i]+array[j];
-			if(s==sum){
-				System.out.println(array[i]+"||"+array[j]);
+        throw new IllegalArgumentException("No two sum solution");
 
-				i++;
-				j--;
-			}else if(s>sum){
-				i++;
-			}else {
-				j--;
-			}
-		}
+    }
+
+	//n
+
+
+	public static int[] twoNumber2(int[] array,int s){
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int [] a =  {1,2};
+        for(int i=0;i<array.length;i++){
+            int expectNum = s-array[i];
+            if(map.containsKey(expectNum)){
+                return new int[]{i, map.get(expectNum)};
+            }
+            map.put(array[i],i);
+
+        }
+        throw new IllegalArgumentException("No two sum solution");
+
 	}
-	
-	//用到二分查找 nlog（n）
-	public void twonumber3(){
-		
-	}
-	
+
+    public static void main(String[] args) {
+        int [] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoNumber(nums,target)));
+        System.out.println(Arrays.toString(twoNumber2(nums,target)));
+
+
+
+    }
+
 
 }
